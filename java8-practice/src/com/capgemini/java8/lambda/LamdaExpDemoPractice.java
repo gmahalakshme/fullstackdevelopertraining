774af7b8c6d18013c2e8a990c5b331c8aa500e14
gravuri <gravuri@DIN66006130.corp.capgemini.com>
2017-11-29 16:11:17 +0530

    changes commited

--- java8-practice/src/com/capgemini/java8/lambda/LamdaExpDemoPractice.java
+++ java8-practice/src/com/capgemini/java8/lambda/LamdaExpDemoPractice.java
@@ -1,10 +1,41 @@
 package com.capgemini.java8.lambda;
+import java.io.File;
+import java.io.FileFilter;
+
 
 public class LamdaExpDemoPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 
+		//using fileFileter interface by using ananymous class instance   
+		
+	/*	FileFilter fileFilter= new FileFilter() {
+			
+			
+			public boolean accept(File file) {
+				return file.getName().endsWith(".java");
+				
+			}
+			
+		};*/
+		
+		//using lamda expression
+				
+		FileFilter lamda=(File file) -> file.getName().endsWith(".java");
+		
+		File dir= new File("D:\\temp");
+		File[] files= dir.listFiles(lamda);
+		for (File f:files) {
+			System.out.println("files"+f);
+		}
+		
+	
+		
+		
+		
+				
+		
 	}
 
 }
